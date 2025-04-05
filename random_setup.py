0f47d5bf4653ff5b0cd9113d05dddd6513b80a82
Alexis CG <113210633+AlexisCottierGuilly@users.noreply.github.com>
2025-04-05 00:05:19 -0400

    More UI

--- random_setup.py
+++ random_setup.py
@@ -1,54 +1,22 @@
 import random
 import math
 
-"""
-The objective is to save some random simulation frames (very simply) in
-the data.txt file.
-The data.txt file contains the following information:
-1. The first line is the board information (for now, a rectangle)
-    The data is stored like this: x1 y1 x2 y2 x3 y3 x4 y4
-    - In the example, 2 vertex are given, so 4 points are given
-2. Each other line is a simulation state
-3. In each line, we get informations about all the balls in the frame
-    - X, Y, Vx, Vy, R
-    These values are separated by a space, and ball infos are separated by a space too
-
-So choose a certain random number of balls
-Set radius, position and velocity randomly
-
-Generate more frames with random (very random) variations of values
-Note that, the positions are always between 0 and 1 (in x and y)
-
-Let's to this!
-"""
-
 # Constants
 NUM_BALLS = random.randint(2, 5)
 NUM_FRAMES = random.randint(5, 10)
 
-frames = [] # List to store the frames [[frame1], [frame2], ...], frame ex. [ball1, ball2, ...], ball ex. [x, y, vx, vy, r]
+# List to store the frames [[frame1], [frame2], ...], frame ex. [ball1, ball2, ...], ball ex. [x, y, vx, vy, r]
+frames = []
 board = []
 
 def random_board():
-    # randomly generate a board with a certain number of points
-    # for now, a rectangle
     board = []  # list of vertex ([[(x1, y1), (x2, y2)], [(x3, y3), (x4, y4)], ...])
-    # for testing, do a rectangle
+
     board = [
         [(0, 0), (1, 0)],  # bottom edge
         [(1, 0), (1, 1)],  # right edge
         [(1, 1), (0, 1)],  # top edge
         [(0, 1), (0, 0)],  # left edge
-    ]
-
-    # make a hexagon
-    board = [
-       [(-0.5, -math.sqrt(3)/2), (0.5, -math.sqrt(3)/2)],  # bottom edge
-         [(0.5, -math.sqrt(3)/2), (1, 0)],  # right edge
-         [(1, 0), (0.5, math.sqrt(3)/2)],  # top edge
-         [(0.5, math.sqrt(3)/2), (-0.5, math.sqrt(3)/2)],  # left edge
-         [(-0.5, math.sqrt(3)/2), (-1, 0)],  # left edge
-         [(-1, 0), (-0.5, -math.sqrt(3)/2)],  # bottom edge
     ]
 
     return board
@@ -66,23 +34,18 @@ def initialize_frame():
     return frame
 
 def new_frame(last_frame):
-    # depending on the speeds of the balls, and ensuring they don't go out of limits,
-    # create a new frame with random variations of the last frame
     new_frame = []
     for ball in last_frame:
-        r = ball[4]  # radius remains the same
-        x = random.uniform(r/100, 1 - r/100)  # random position
+        r = ball[4]
+        x = random.uniform(r/100, 1 - r/100)
         y = random.uniform(r/100, 1 - r/100)
-        vx = ball[2] + random.uniform(-0.5, 0.5)  # random variation
-        vy = ball[3] + random.uniform(-0.5, 0.5)  # random variation
+        vx = ball[2] + random.uniform(-0.5, 0.5)
+        vy = ball[3] + random.uniform(-0.5, 0.5)
         new_frame.append([x, y, vx, vy, r])
     return new_frame
 
 def save_to_file(_board, _frames, filename='data.txt'):
-    # clear file and write down as explained above
     with open(filename, 'w') as file:
-        # write the board information with spaces
-        # really random enormous shape (open in the center and no crossings)
         board_str = ' '.join([' '.join(map(str, point)) for edge in _board for point in edge])
         file.write(board_str + '\n')
         for frame in _frames:

95a37088149c0a8c299966cef3f25b761d3e5144
Alexis CG <113210633+AlexisCottierGuilly@users.noreply.github.com>
2025-04-05 01:37:26 -0400

    bouton pause

--- animation_testing.py
+++ animation_testing.py
@@ -7,6 +7,8 @@ import matplotlib.patches as patches
 import matplotlib as mpl
 import matplotlib.font_manager as fm
 
+from matplotlib.widgets import Button
+
 fe = fm.FontEntry(
     fname='Spinnaker-Regular.ttf',
     name='Spinnaker')
@@ -17,6 +19,35 @@ BOARD_SIZE = (100, 100)
 
 SUB_FRAMES = 60
 ANIMATION_SPEED = 1
+PAUSE_BUTTON_PATH = "icons/pause.png"
+PLAY_BUTTON_PATH = "icons/play.png"
+
+play_img = plt.imread(PLAY_BUTTON_PATH)
+pause_img = plt.imread(PAUSE_BUTTON_PATH)
+
+need_to_pause = False
+need_to_play = False
+
+click_start = None
+
+
+class Ball(plt.Circle):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.trail_items = []
+        self.selected = False
+
+        self.dx = 0
+        self.dy = 0
+    
+    def select(self):
+        self.selected = True
+        self.set_edgecolor('red')
+        self.set_linewidth(3)
+    def unselect(self):
+        self.selected = False
+        self.set_edgecolor('black')
+        self.set_linewidth(0)
 
 
 def read_data(file_path):
@@ -98,20 +129,14 @@ def get_board(board_vertices, size=(100, 100)):
     
     scaled_vertices = [((v[0] + offset_x) * scale_x, (v[1] + offset_y) * scale_y) for v in board_vertices]
     
-    polygon = plt.Polygon(scaled_vertices, closed=True, edgecolor='white', facecolor='none')
+    polygon = plt.Polygon(scaled_vertices, closed=True, edgecolor='saddlebrown', facecolor='darkgreen', alpha=0.5)
+    polygon.set_linewidth(5)
     return polygon
 
 
 def update(frame1, frame2, percentage):
-    modified_patches = []
-    nb_balls = len(ball_patches) / (NB_OF_TRAIL_ELEMENTS + 1)
-    elems_per_ball = NB_OF_TRAIL_ELEMENTS + 1
-    for i in range(int(nb_balls)):
-        modified_patches.append((ball_patches[i * elems_per_ball], ball_patches[i * elems_per_ball + 1:i * elems_per_ball + elems_per_ball]))
-
-    for i, ball_info in enumerate(modified_patches):
-        ball = ball_info[0]
-        trail_items = ball_info[1]
+    for i, ball in enumerate(balls):
+        trail_items = ball.trail_items
         x1, y1, vx1, vy1, r1 = frame1[i]
         x2, y2, vx2, vy2, r2 = frame2[i]
 
@@ -130,31 +155,108 @@ def update(frame1, frame2, percentage):
         color = plt.cm.viridis(speed) / np.sqrt(2)
         ball.set_color(color)
 
+        ball.vx, ball.vy = vx, vy
+
+        if ball.selected:
+            ball.select()
+        else:
+            ball.unselect()
+
         update_trail_items(trail_items, x, y, vx, vy, r, color)
 
     return ball_patches
 
 
 def animate(i):
+    global need_to_pause, need_to_play
     frame1_index = i // SUB_FRAMES
     frame2_index = (i // SUB_FRAMES + 1) % len(frames)
     percentage = (i % SUB_FRAMES) / SUB_FRAMES
 
     update(frames[frame1_index], frames[frame2_index], percentage)
+
+    if need_to_pause:
+        ani.event_source.stop()
+        need_to_pause = False
+    if need_to_play:
+        ani.event_source.start()
+        need_to_play = False
+
     return ball_patches
+
+
+def process_force_modification(ball, mouse_pos):
+    mx, my = mouse_pos
+    x, y = ball.get_center()
+    dx = mx - x
+    dy = my - y
+
+    multiplier = 0.05
+
+    angle = np.arctan2(dy, dx)
+    distance = np.sqrt(dx**2 + dy**2)
+
+    ball.vx = np.cos(angle) * distance * multiplier
+    ball.vy = np.sin(angle) * distance * multiplier
+
+
+def pause_request():
+    global need_to_pause
+    need_to_pause = True
+def play_request():
+    global need_to_play
+    need_to_play = True
 
 
 def on_press(event):
     global is_paused
     if event.key == ' ':  # la barre d'espace pour la pause/reprise
-        if is_paused:
-            ani.event_source.start()
-            is_paused = False
-            print("Reprise de l'animation !")
-        else:
-            ani.event_source.stop()
-            is_paused = True
-            print("Animation en pause...")
+        toggle_animation(event)
+    if event.key == 'r':
+        for ball in balls:
+            if ball.selected:
+                ball.unselect()
+            else:
+                ball.select()
+        fig.canvas.draw()
+
+
+def on_mouse_press(event):
+    global click_start
+    if event.inaxes:
+        click_start = (event.xdata, event.ydata)
+
+
+def on_mouse_release(event):
+    global click_start
+    click_start = None
+
+
+def on_mouse_drag(event):
+    if click_start is not None and event.inaxes:
+        did_find_selected_ball = False
+        for ball in balls:
+            if ball.selected:
+                did_find_selected_ball = True
+                process_force_modification(ball, (event.xdata, event.ydata))
+                update_trail_items(ball.trail_items, ball.get_center()[0], ball.get_center()[1], ball.vx, ball.vy, ball.get_radius(), ball.get_facecolor())
+        if did_find_selected_ball:
+            fig.canvas.draw()
+
+
+def toggle_animation(event):
+    global is_paused
+    if is_paused:
+        ani.event_source.start()
+        button_ax.images[0].set_data(pause_img)
+        fig.canvas.draw()
+        play_request()
+    else:
+        ani.event_source.stop()
+        button_ax.images[0].set_data(play_img)
+        fig.canvas.draw()
+        pause_request()
+    is_paused = not is_paused
 
 
 balls = []
@@ -170,7 +272,7 @@ for i in range(num_balls):
 plt.style.use('dark_background')
 fig, ax = plt.subplots()
 
-fig.patch.set_facecolor('#151515')
+# fig.patch.set_facecolor('#151515')
 fig.canvas.mpl_connect('key_press_event', on_press)
 
 ax.set_xlim(-1, BOARD_SIZE[0] + 1)
@@ -188,24 +290,34 @@ ax.spines['bottom'].set_visible(False)
 board = get_board(board_vertices, size=BOARD_SIZE)
 ax.add_patch(board)
 
-# Pause/Play button at the bottom right of the screen
-pause_button = patches.Rectangle((BOARD_SIZE[0] - 1, 0), 1, 1, color='white', alpha=0.5)
-ax.add_patch(pause_button)
-ax.text(BOARD_SIZE[0] - 0.5, 0.5, 'Pause', color='white', fontsize=12, ha='center', va='center')
+# Create an axis for the image button in center of the figure (bottom)
+button_ax = fig.add_axes([0.48, 0.02, 0.075, 0.075], frameon=False)
+pause_button = Button(button_ax, '', color='black', hovercolor='gray')  # empty label
+
+# Load the image
+button_ax.imshow(pause_img)
+button_ax.axis('off')  # Hide ticks and axes for a clean look
+pause_button.on_clicked(toggle_animation)
 
 ball_patches = []
 NB_OF_TRAIL_ELEMENTS = 1
 for i in range(num_balls):
-    ball = plt.Circle((0, 0), 1, color=ball_colors[i])
+    ball = Ball((0, 0), 1, color=ball_colors[i])
     ax.add_patch(ball)
     trail = create_trail_items(ball, ax)
+    ball.trail_items = trail
+    balls.append(ball)
     ball_patches.append(ball)
     for t in trail:
         ball_patches.append(t)
 
 interval = 1000 / 60 / ANIMATION_SPEED
 number_of_frames = len(frames) * SUB_FRAMES
-ani = animation.FuncAnimation(fig, animate, frames=number_of_frames, interval=interval, blit=True)
+ani = animation.FuncAnimation(fig, animate, frames=number_of_frames, interval=interval, blit=False)
 ani.event_source.start()
+
+fig.canvas.mpl_connect('button_press_event', on_mouse_press)
+fig.canvas.mpl_connect('button_release_event', on_mouse_release)
+fig.canvas.mpl_connect('motion_notify_event', on_mouse_drag)
 
 plt.show()
